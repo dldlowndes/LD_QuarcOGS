4d3574325f57270d71d33baa4d7f3d01f6d72831
dukederek <dukederek@gmail.com>
2020-06-12 15:38:38 +0100

    used static analysis, took some of its recommendations on board. no functionality changes.

--- LD_PassFinder.py
+++ LD_PassFinder.py
@@ -1,34 +1,13 @@
-import datetime
-import dateutil
-import pytz
-import tzlocal
-import warnings
-
-import astropy
-import astropy.coordinates
-import astropy.time
-
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import scipy.signal
-
-import sgp4.api
-
-import LD_MyTLE
-import LD_TLEList
-
 """
 For calculating the alt/az of satellite(s) from a given location. Intended for
 testing the Quarc OGS where we will want to test satellite tracking. Probably
-for this load Celestrak's "visual.txt" list of TLEs. 
+for this load Celestrak's "visual.txt" list of TLEs.
 
 Usage:
     - Make an instance of this class.
     - Set the position of the OGS (latitude, longitude, height)
     - Set the time range of interest using ISO8601 date time strings.
-    - Pass one/some/no TLEs to Caluclate_Passes(). If none are passed, it 
+    - Pass one/some/no TLEs to Caluclate_Passes(). If none are passed, it
     calculates for ALL the satellites in its internal list (this can be a lot!)
     - Use the alt/az vs time data sets for each TLE to choose which to track
 
@@ -42,11 +21,34 @@ TODO: Return the most visible satellite *right now*
 TODO: Some sort of scheduler that builds a list of satellites to track sequentially.
 """
 
+import datetime
+import warnings
+
+import astropy
+import astropy.coordinates
+import astropy.time
+import dateutil
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy.signal
+import sgp4.api
+import tzlocal
+
+import LD_MyTLE
+import LD_TLEList
+
+
 class LD_PassFinder:
+    """
+    Calculates passes of satelites.
+    """
     def __init__(self):
         self.here = None
 
         self.tle_List = None
+
 
     def Set_Position(self, lat, long, height):
         """
@@ -64,11 +66,11 @@ class LD_PassFinder:
         Provide ISO8601 compatible date&time strings for start/stop.
         (or python datetime objects)
         Provide interval in minutes (can accept float for smaller intervals)
-        
+
         The datetime strings should be in whatever the time zone of the
         computer running this. They are converted internally to UTC from the
         local time zone for the algorithms.
-        """ 
+        """
 
         # Interpret the time stamp input and convert into UTC astropy Time objects
         self.t_start = self._Timestamp_Convert(t_start)
@@ -98,7 +100,7 @@ class LD_PassFinder:
         # Seems sensible that the user inputs the datetime in their local time
         # so step 1 of interpreting is getting said time zone.
         self.my_tz = tzlocal.get_localzone()
-        
+
         # Figure out if a ISO8601 string or datetime object were supplied.
         if isinstance(stamp, str):
             stamp_local = self.my_tz.localize(dateutil.parser.parse(stamp))
@@ -106,7 +108,7 @@ class LD_PassFinder:
             stamp_local = self.my_tz.localize(stamp)
         else:
             raise TypeError
-            
+
         # And make into an astropy time object (in UTC)
         return astropy.time.Time(stamp_local)
 
@@ -115,7 +117,7 @@ class LD_PassFinder:
         Return the time range set previously converted back into the local
         time zone.
         """
-        
+
         # Sorry, this is kind of horrible.
         # Converts each astropy time object into a utc datetime object, then
         # converts into the local time zone.
@@ -128,7 +130,7 @@ class LD_PassFinder:
         Celestrak.
         Or provide a LD_TLEList object directly.
         """
-        
+
         if isinstance(tle_List, str):
             self.tle_List = LD_TLEList.LD_TLEList(tle_List)
         elif isinstance(tle_List, LD_TLEList.LD_TLEList):
@@ -141,7 +143,7 @@ class LD_PassFinder:
             warnings.warn("tle_List provided was neither a string, nor a LD_TLEList object. Nothing happened")
 
         return self.tle_List
-    
+
     def Search_TLE_Data(self, search_String):
         """
         Search for the search_String in the names of all of the satellites in
@@ -171,7 +173,6 @@ class LD_PassFinder:
         # Get the cartesian coordinates (and speeds) of each satellite at each
         # point in the time series defined by Set_Time_Range().
         # Then convert to ITRS (which is apparently more standard than TEME)
-        itrs_Data = []
         self.altaz_Data = []
         self.errors = []
         observer = astropy.coordinates.AltAz(location=self.here,
@@ -184,7 +185,7 @@ class LD_PassFinder:
             sat = sgp4.api.Satrec.twoline2rv(tle[1], tle[2])
             e, p, v = sat.sgp4_array(self.jd_Range, np.zeros_like(self.jd_Range))
             if isinstance(e, int):
-                if e !=0:
+                if e != 0:
                     warnings.warn(f"Error with {tle.name}. Skipping")
                     self.errors.append([tle.name, e])
                     continue
@@ -194,7 +195,7 @@ class LD_PassFinder:
                     self.errors.append([tle.name, e])
                     continue
 
-            # SGP4 gives results in some weird coordinate basis (True Equator 
+            # SGP4 gives results in some weird coordinate basis (True Equator
             # Mean Equinox frame (TEME)). Get values in the right format to
             # convert to something more intelligible and useful.
             teme_p = astropy.coordinates.CartesianRepresentation(p * astropy.units.km, xyz_axis=1)
@@ -202,10 +203,10 @@ class LD_PassFinder:
 
             # Put the coordinates into astropy, which can convert between bases
             # using International Terrestrial Reference System (ITRS) coordinates.
-            teme = astropy.coordinates.TEME(teme_p.with_differentials(teme_v), obstime = self.utc_Time_Series)
+            teme = astropy.coordinates.TEME(teme_p.with_differentials(teme_v), obstime=self.utc_Time_Series)
             itrs = teme.transform_to(astropy.coordinates.ITRS(obstime=self.utc_Time_Series))
 
-            # Convert satellite coordinates (relative to earth) into alt/az 
+            # Convert satellite coordinates (relative to earth) into alt/az
             # from this position on earth (set by Set_Position).
             view = itrs.transform_to(observer)
             data = pd.DataFrame({
@@ -219,13 +220,13 @@ class LD_PassFinder:
             print(f"Some errors, see {self.errors}")
 
         return self.altaz_Data
-        
+
     def Filter_Passes(self, alt_Filter):
         """
         Filter out all of the data from the passes where the satellite peaks
         below "alt_Filter" degrees altitude.
         """
-        
+
         # Container for the pass data, each line will comprise the satellite
         # [name, [peak time, peak alt, az@peak], pass data] where pass data is
         # alt/az values for when the satellite is above the horizon.
@@ -234,16 +235,16 @@ class LD_PassFinder:
             # Find the local maxima in the each satellite's tracks (there may
             # be more than one!)
             alt_peaks = scipy.signal.argrelextrema(data["alt"].values, np.greater)[0]
-        
+
             # Look at each peak separately.
             for peak in alt_peaks:
                 # Get the data at the peak
                 time, alt, az = data.iloc[peak]
-                
+
                 # If the peak is high enough to be worth trying to look at.
                 if alt > alt_Filter:
-                    
-                    # First, scan forwards to find when the satellite drops 
+
+                    # First, scan forwards to find when the satellite drops
                     # back below the horizon. This finds the end of the pass.
                     i = peak
                     search_alt = alt
@@ -253,7 +254,7 @@ class LD_PassFinder:
                             break
                         search_alt = data.iloc[i]["alt"]
                     end = (i - 1)
-                    
+
                     # Then scan backwards from the peak to find when the
                     # satellite first came up over the horizon to find the
                     # start of the pass
@@ -264,26 +265,26 @@ class LD_PassFinder:
                             break
                         search_alt = data.iloc[i]["alt"]
                     start = (i + 1)
-                    
+
                     # Extract the portion of the satellite's track data where
                     # it was above the horizon *this time*
                     pass_Isolated = data[start:end]
-                    
+
                     # Add to the rest.
                     # NOTE datetime is converted from UTC back to local TZ here!
                     peak_Info = [time.to_datetime(self.my_tz), alt, az]
                     self.pass_Data.append([sat, peak_Info, pass_Isolated])
-        
+
         # Sort the passes into chronological order (by peak time), this makes
         # display and plotting easier and prettier.
         self.pass_Data.sort(key=lambda x: x[1][0])
         return self.pass_Data
-    
+
     def Get_Pass_List(self):
         """
         Return a neat list of just the TLE, peak time, peak alt, az@peak
         """
-        
+
         pass_List = []
         for sat in self.pass_Data:
             name = sat[0].name
@@ -297,8 +298,8 @@ class LD_PassFinder:
                 "az": az
                 })
         return pd.DataFrame(pass_List)
-    
-    def Print_Pass_List(self):        
+
+    def Print_Pass_List(self):
         """
         Print each satellite pass as identified by Filter_Passes(). These are
         already in chronological order so no need to do that.
@@ -309,12 +310,12 @@ class LD_PassFinder:
         printable = data.to_string()
         print(printable)
         return printable
-        
+
     def Save_Pass_List(self):
         """
         As with Print_Pass_List(), but dump out to a datestamped csv file.
         """
-        
+
         # Generate the file name which contains the start and stop times of the
         # scan.
         t_fmt = "%Y-%m-%dT%H-%M-%S"
@@ -324,12 +325,12 @@ class LD_PassFinder:
                             self.t_stop.to_datetime(self.my_tz).strftime(t_fmt),
                             ".csv"
                             ])
-        
+
         with open(filename, "w") as f:
-            f.write(f"Name, time, alt, az\n")
+            f.write("Name, time, alt, az\n")
             data = self.Get_Pass_List()
-            
-            for i, item in data.iterrows():
+
+            for _, item in data.iterrows():
                 f.write(f"{item.satellite}, {item.time}, {item.alt}, {item.az}\n")
         return data.to_string()
 
@@ -340,58 +341,51 @@ class LD_PassFinder:
         just crash.
         """
         fig, my_ax = plt.subplots()
-        my_ax.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M:%S"))
+        my_ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%H:%M:%S"))
         for sat, data in self.altaz_Data:
             data.plot(x="time", y="alt", ax=my_ax, rot=90)
         plt.show()
-        
-        
+
+
     def Plot_Good_Passes(self):
         """
         Plot all passes identified by Filter_Passes() on the same axis.
-        
-        TODO: Prettier
-        TODO: Labelling
-        TODO: Interactive?
         """
 
         # Make an axis that can be repeatedly plotted onto
         fig, my_ax = plt.subplots()
-        
+
         for sat, peak_Info, data in self.pass_Data:
-            # TODO: There must be a more elegant way to deal with this!
             # Make timestamps that matplotlib can understand.
-            # x_data = [matplotlib.dates.date2num(x.to_datetime(self.my_tz))
-            #                                     for x in data["time"].values]
             x_data = [x.plot_date for x in data["time"].values]
             # Altitude is the only particularly interesting feature for these
             # purposes (plotting azimuth as well would be confusing)
             y_data = data["alt"]
             my_ax.plot_date(x_data, y_data, xdate=True,
                             linestyle="-", marker=None)
-            
+
             label_text = sat.name.rstrip()
             label_pos_x = matplotlib.dates.date2num(peak_Info[0])
             label_pos_y = peak_Info[1]
-            
+
             my_ax.text(label_pos_x, label_pos_y, label_text)
-        
+
         my_ax.xaxis_date(self.my_tz)
         fmt = matplotlib.dates.DateFormatter("%Y/%m/%d %H:%M")
         my_ax.xaxis.set_major_formatter(fmt)
-        
+
         # Get the axes looking nice.
         my_ax.xaxis.set_major_locator(matplotlib.dates.MinuteLocator(interval=15))
         my_ax.grid(True, which="major", axis="both", linestyle="--")
-        
+
         my_ax.xaxis.set_minor_locator(matplotlib.dates.MinuteLocator(interval=5))
         my_ax.grid(True, which="minor", axis="both", linestyle=":")
-        
+
         my_ax.xaxis.set_tick_params(rotation=90)
 
         my_ax.set_ylim(0, 90)
         my_ax.set_xlim(self.t_start.plot_date, self.t_stop.plot_date)
-        
+
         fig.show()
 
 if __name__ == "__main__":
@@ -401,17 +395,17 @@ if __name__ == "__main__":
     # Set the time range to search for passes within.
     # (and the resolution in minutes)
     finder.Search_Time_Range(
-            "2020-06-11T22:00:00",
-            "2020-06-12T06:00:00",
-            1
-            )
-    
+        "2020-06-11T22:00:00",
+        "2020-06-12T06:00:00",
+        1
+        )
+
     # Load the TLE data file into the finder.
-    finder.Load_TLE_Data("tle_Files/visual.txt") 
+    finder.Load_TLE_Data("tle_Files/visual.txt")
     #finder.Load_TLE_Data("tle_Files/active.txt") # a very big list!
     #finder.Load_TLE_Data("tle_Files/cubesat.txt")
     #finder.Load_TLE_Data() # Go get Celestrak's "active" list from the internet.
-    
+
     # Find some values in the TLE list.
     # Find TLE lists on:
     #   - https://www.celestrak.com/NORAD/elements/
@@ -420,15 +414,15 @@ if __name__ == "__main__":
     t1 = finder.Search_TLE_Data("zarya") # just the ISS
     t2 = finder.Search_TLE_Data("starlink") # all the starlink satellites (!)
     t3 = finder.Search_TLE_Data("cosmos")
-    
+
     # Calculate the alt/az (degrees) at the time intervals specified for all
     # the satellites passed in. Outputs in the format [<tle_Obj>, <alt>, <az>]
-    data = finder.Calculate_Passes(t3)
-    
+    pdata = finder.Calculate_Passes(t3)
+
     #finder.Plot_Passes()
-    
+
     viable_Passes = finder.Filter_Passes(0)
     finder.Print_Pass_List()
     finder.Save_Pass_List()
-    
+
     finder.Plot_Good_Passes()
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ TODO:
 import datetime
 import sys
 
-from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5 import QtWidgets, QtCore
 import matplotlib
 from matplotlib.backends.backend_qt5agg import (
         FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
@@ -22,7 +22,7 @@ from matplotlib.figure import Figure
 
 import mainwindow
 import satellites_Thread
-import telescope_Thread
+#import telescope_Thread
 
 class MyWindow(QtWidgets.QMainWindow):
     """
@@ -32,22 +32,22 @@ class MyWindow(QtWidgets.QMainWindow):
         super(MyWindow, self).__init__()
         self.ui = mainwindow.Ui_MainWindow()
         self.ui.setupUi(self)
-        
+
         # Init various components
         self.Init_Defaults()
         self.Init_Threads()
         self.Init_Connections()
         self.Init_Plot()
-        
+
     def Init_Defaults(self):
         """
         Set some sensible defaults to populate the GUI. Help reduce the
         amount of typing.
         """
-        
+
         # The TLE file name (Celestrak's visual list seems good enough)
         self.ui.value_Filename.setText("tle_Files/visual.txt")
-        
+
         # Queens building. I'm making a statement.
         self.ui.value_Lat.setText("51.4566")
         self.ui.value_Lon.setText("-2.6020")
@@ -61,128 +61,123 @@ class MyWindow(QtWidgets.QMainWindow):
         self.ui.date_Start.setDateTime(QtCore.QDateTime(t_start))
         self.ui.date_Stop.setDateTime(QtCore.QDateTime(t_stop))
         self.ui.value_Resolution.setValue(1)
-        
+
         # 30 degrees high enough?
         self.ui.value_Degrees.setValue(30)
 
-    def Init_Threads(self):    
+    def Init_Threads(self):
         """
         Threads to do long lasting work that would hang the GUI.
         Make threads and signal/slot connections in here.
         """
         self.satellites_Thread = satellites_Thread.satellites_Thread()
-        
+
         # For when the satellites thread has loaded some TLEs for passing to
         # the GUI
         self.satellites_Thread.tles_Signal.connect(self.On_TLEs_Signal)
-        
+
         # For when the viable passes have been calculated.
         self.satellites_Thread.passes_Signal.connect(self.On_Passes_Signal)
 
     def Init_Connections(self):
         """
-        Connect GUI elements to functions.        
+        Connect GUI elements to functions.
         """
 
         self.ui.button_LoadFile.clicked.connect(self.On_Load_Button)
         self.ui.button_Search.clicked.connect(self.On_Search_Button)
         self.ui.button_Process.clicked.connect(self.On_Process_Button)
-        
+
     def Init_Plot(self):
+        """
+        Make the canvas and set some things that can be set at init time
+        (which turns out to be not a lot!)
+        """
+
         self.canvas = FigureCanvas(Figure(figsize=(5, 3)))
         self.ui.layout_Graph.addWidget(NavigationToolbar(self.canvas, self))
         self.ui.layout_Graph.addWidget(self.canvas)
         self.axes = self.canvas.figure.subplots()
         self.canvas.figure.subplots_adjust(left=0.05,
-                                    right=0.95,
-                                    top=0.98,
-                                    bottom=0.175)
-        
-        # self.axes.set_xlim(-1, 1)
-        # self.axes.set_ylim(-1, 1)
-        # self.axes.minorticks_on()
-        # self.axes.grid(which="major",
-        #                 linestyle="-",
-        #                 linewidth=0.5,
-        #                 color="black")
-        # self.axes.grid(which="minor",
-        #                 linestyle=":",
-        #                 linewidth=0.5,
-        #                 color="grey")
-        
+                                           right=0.95,
+                                           top=0.98,
+                                           bottom=0.175)
+
     def Update_Plot(self, pass_Data):
+        """
+        Plot all the passes on a graph window. alt against time for each sat
+        that fulfilled the criteria of Filter_Passes in the satellites_Thread.
+        """
+
         self.axes.xaxis_date(self.satellites_Thread.finder.my_tz)
         #fmt = matplotlib.dates.DateFormatter("%Y/%m/%d %H:%M")
         fmt = matplotlib.dates.DateFormatter("%H:%M")
         self.axes.xaxis.set_major_formatter(fmt)
-        
+
         # Get the axes looking nice.
         self.axes.xaxis.set_major_locator(matplotlib.dates.MinuteLocator(interval=30))
         self.axes.grid(True, which="major", axis="both", linestyle="--")
-        
+
         self.axes.xaxis.set_minor_locator(matplotlib.dates.MinuteLocator(interval=5))
         self.axes.grid(True, which="minor", axis="both", linestyle=":")
-        
+
         self.axes.xaxis.set_tick_params(rotation=90)
 
         self.axes.set_ylim(0, 90)
         self.axes.set_xlim(self.satellites_Thread.finder.t_start.plot_date,
-                       self.satellites_Thread.finder.t_stop.plot_date)
-        
+                           self.satellites_Thread.finder.t_stop.plot_date)
+
         for sat, peak_Info, data in pass_Data:
-            # TODO: There must be a more elegant way to deal with this!
             # Make timestamps that matplotlib can understand.
-            # x_data = [matplotlib.dates.date2num(x.to_datetime(self.my_tz))
-            #                                     for x in data["time"].values]
             x_data = [x.plot_date for x in data["time"].values]
             # Altitude is the only particularly interesting feature for these
             # purposes (plotting azimuth as well would be confusing)
             y_data = data["alt"]
             self.axes.plot_date(x_data, y_data, xdate=True,
                                 linestyle="-", marker=None)
-            
+
             label_text = sat.name.rstrip()
             label_pos_x = matplotlib.dates.date2num(peak_Info[0])
             label_pos_y = peak_Info[1]
-            
+
             self.axes.text(label_pos_x, label_pos_y, label_text)
-        
+
         self.canvas.draw()
-        
+
     def On_Load_Button(self):
         """
         Load a TLE list from file.
         """
-        
+
         self.satellites_Thread.Load_List(self.ui.value_Filename.text())
-        
+
     def On_Search_Button(self):
         """
         Search through the TLEs and return only those whose name contains
         the text in the search box
         """
-        
+
         search = self.ui.value_Search.text()
         self.satellites_Thread.Search_List(search)
-        
+
     def On_Process_Button(self):
         """
         Find the passes over the current site above some threshold alt.
         """
-        
+
         # Site information
         self.satellites_Thread.lat = float(self.ui.value_Lat.text())
         self.satellites_Thread.lon = float(self.ui.value_Lon.text())
         self.satellites_Thread.height = float(self.ui.value_Height.text())
-        
+
         # Times to search for passes within (and resolution)
         self.satellites_Thread.t_start = self.ui.date_Start.dateTime().toString(QtCore.Qt.ISODate)
         self.satellites_Thread.t_stop = self.ui.date_Stop.dateTime().toString(QtCore.Qt.ISODate)
         self.satellites_Thread.t_step = self.ui.value_Resolution.value()
-        
+
         # Filter out passes that peak below this alt
         self.satellites_Thread.degrees = self.ui.value_Degrees.value()
-        
+
         # Clear the passes table since we're about to start reprocessing.
         # Persisting data would be confusing since they might be for different
         # parameters than shown in the GUI.
@@ -200,28 +195,28 @@ class MyWindow(QtWidgets.QMainWindow):
         """
         Display the TLE list contents in the GUI
         """
-        
+
         # Clear table (since the TLE list is cleared anyway)
         self.ui.table_TLEs.setRowCount(0)
-        
+
         # Fill table with new data.
         for i, tle in enumerate(tle_List):
             self.ui.table_TLEs.insertRow(i)
             self.ui.table_TLEs.setItem(i, 0, QtWidgets.QTableWidgetItem(tle.name))
-            self.ui.table_TLEs.setItem(i, 1,  QtWidgets.QTableWidgetItem(tle[1]))
+            self.ui.table_TLEs.setItem(i, 1, QtWidgets.QTableWidgetItem(tle[1]))
             self.ui.table_TLEs.setItem(i, 2, QtWidgets.QTableWidgetItem(tle[2]))
-        
-    
+
+
     def On_Passes_Signal(self, pass_Data):
         """
         Fill the table in the passes tab (and the graph tab?)
         """
         # No need to clear passes since the list is cleared when the processing
         # starts.
-        
+
         # This signal means the processing is finished so update GUI to say.
         self.ui.label_Status.setText("Status: Inactive")
-        
+
         # Fill the table with new data.
         for i, sat in enumerate(pass_Data):
             name = sat[0].name.rstrip()
@@ -233,7 +228,7 @@ class MyWindow(QtWidgets.QMainWindow):
             self.ui.table_Passes.setItem(i, 1, QtWidgets.QTableWidgetItem(f"{alt:0.2f}")) #alt
             self.ui.table_Passes.setItem(i, 2, QtWidgets.QTableWidgetItem(time)) #time
             self.ui.table_Passes.setItem(i, 3, QtWidgets.QTableWidgetItem(f"{az:0.2f}")) #az
-        
+
         self.Update_Plot(pass_Data)
 
 app = QtWidgets.QApplication([])
